919b9175cad4bad6106a27ab1f145f5910a0093b
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2023-02-01 19:14:55 -0500

    updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,15 @@
 // const sequelize = require("../config/connection");
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 const { Post, User, Comment } = require("../models");
-const session = require("express-session");
+// const session = require("express-session");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-    const postData = await Post.findAll({ include: User });
+    const postData = await Post.findAll({ include: [User] });
     const posts = postData.map((posts) => posts.get({ plain: true }));
     res.render("homepage", {
-      posts,
-      logged_in: req.session.logged_in,
+      posts
     });
   } catch (err) {
     res.status(500).json(err);
@@ -22,66 +21,25 @@ router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include:[
+        User,
         {
-        model: Comment, include: {
-        model: User,
-        attributes: ['id', 'username']
-         }
+        model: Comment, include: [User]
+         
          },
-         {
-         model: 
-         User,
-         attributes: ['id','username']
-       }
     ]
 });
-
+if(postData){
   const post = postData.get({ plain: true });
-        res.render('post', {
-          ...post,
-          logged_in: req.session.logged_in,
-  })
+  res.render('post', {
+    post
+})
+}else{
+  res.status(404).end()
+}  
 } catch (err) {
   console.log('something went wrong')
-  res.status(400).json(err);
+  res.status(500).json(err);
  }
-});
-
-//find by the Post
-router.get('/post', async (req, res) => {
-    try {
-      const postData = await Post.findAll ({ include: User })
-
-      const posts = postData.map((posts) => posts.get({ plain: true }));
-      res.render('post', {
-      posts,
-      logged_in: req.session.logged_in,
-})
-  }  catch (err) {
-     res.status(500).json(err)
-    }
-});
-
-// Route to user's dashboard by ID
-router.get('/dashboard', withAuth, async (req, res) => {
-    try {
-      console.log(req.session.user_id)
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Post }],
-      });
-      const user = userData.get({ plain: true });
-
-    //   console.log(user)
-
-      res.render('dashboard', {
-        ...user,
-        logged_in: true
-      });
-      
-    } catch (err) {
-      res.status(500).json(err);
-    }
 });
 
 // Route to sign up
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const dashRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashRoutes);
 
 
 
